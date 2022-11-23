b8cf8438962bf124bf052b38bd814c10b3e19e36
Rehan-Shah12 <100332064+Rehan-Shah12@users.noreply.github.com>
2022-11-23 12:15:52 +0500

    Error in authentication ... forbidden

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
+require("dotenv").config;
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
 var bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Get All
 router.get("/", async (req, res) => {
@@ -36,26 +38,79 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
-  const { name, password } = req.body;
-  User.findOne({ name: name }).then((savedUser) => {
-    if (!savedUser) {
-      res.status(422).json({ error: "Invalid Email or password" });
-    }
-  });
-  // if (user == null) {
-  //   return res.status(400).send("Cannot find user");
-  // }
-  try {
-    if (await bcrypt.compare(req.body.password, { password: password })) {
-      // Yaha pr ghalti hai
-      res.send("Success");
-    } else {
-      res.send("Not Allowed");
-    }
-  } catch (error) {
-    res.status(500).send();
-  }
+// router.post("/login", async (req, res) => {
+//   const { name, password } = req.body;
+//   User.findOne({ name: name }).then((savedUser) => {
+//     if (!savedUser) {
+//       res.status(422).json({ error: "Invalid Email or password" });
+//     }
+//   });
+//   try {
+//     if (await bcrypt.compare(req.body.password, { password: password })) {
+//       // Yaha pr ghalti hai
+//       res.send("Success");
+//     } else {
+//       res.send("Not Allowed");
+//     }
+//   } catch (error) {
+//     res.status(500).send();
+//   }
+// });
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+router.post("/post", authenticationToken, (req, res) => {
+  res.json(User.filter((post) => post.username == req.body.name));
 });
 
+router.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+
+  // User.findOne({ name: name }, (err, returnedUser) => {
+  //   if (err || !returnedUser) {
+  //     return res.status(400).json({
+  //       error: "Name already Exists",
+  //     });
+  //   }
+  //   bcrypt
+  //     .compare(password, returnedUser.password)
+  //     .then((result) => {
+  //       if (!result) {
+  //         return res.status(400).json({
+  //           error: "Wrong password",
+  //         });
+  //       }
+
+  //       res.json({ returnedUser });
+  //     })
+  //     .catch((err) => {
+  //       return res.status(400).json({
+  //         error: err,
+  //       });
+  //     });
+
+  // Authentication
+
+  // const username = req.body.name;
+  // const user = { name: username };
+
+  const accessToken = jwt.sign(
+    { name: name },
+    `${process.env.ACCESS_TOKEN_SECRET}`
+  );
+  res.json({ accessToken: accessToken });
+});
+// });
+
+function authenticationToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 module.exports = router;
